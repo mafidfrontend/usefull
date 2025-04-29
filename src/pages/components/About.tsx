3b718e3d1df9438ea56a478859f3d86b9e29d3a1
mafidfrontend <neo.uz001@gmail.com>
2025-04-29 17:01:33 +0500

    image with ant design

--- src/pages/components/About.tsx
+++ src/pages/components/About.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { Image } from "antd";
 import React from "react";
 
 function About() {
--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { Image } from "antd";
 import Link from "next/link";
 import React from "react";
 
@@ -6,28 +6,28 @@ function Navbar() {
     return (
         <nav className="container-fluid">
             <div className="container-xl">
-                <a href="index.html" className="logo">
+                <Link href={"/"} className="logo">
                     <Image src="images/logo.png" alt="" />
-                </a>
+                </Link>
                 <Link href={"/"}>Home</Link>
                 <a href="#">Studio</a>
                 <a href="#">Services</a>
                 <a href="#">Contact</a>
                 <a href="#">FAQ`s</a>
-                <a href="mailto:neo.uz001@gmail.com" className="btn_1">
-                    Let&apos;s talk
+                <a href="mailto:hellow.ashutosh@gmail.com" className="btn_1">
+                    Let's talk
                 </a>
                 <div className="menu">
                     <Image src="images/hamburger.png" alt="" />
                 </div>
                 <div className="menu_container">
-                    <Link href={"/"}>Home</Link>
+                    <Link href="#">Home</Link>
                     <a href="#">Studio</a>
                     <a href="#">Services</a>
                     <a href="#">Contact</a>
                     <a href="#">FAQ`s</a>
-                    <a href="mailto:neo.uz001@gmail.com" className="btn_1">
-                        Let&apos;s talk
+                    <a href="mailto:hellow.ashutosh@gmail.com" className="btn_1">
+                        Let's talk
                     </a>
                 </div>
             </div>
